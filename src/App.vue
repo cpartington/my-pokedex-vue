2d690cc7f7ff68a865c7cfa3aed90924da207efa
Christie Partington <christiedpartington@gmail.com>
2020-03-02 17:10:57 -0700

    Update with pokedex styling and content

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,69 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div class="header-bar">
+      <div class="header-content">
+        <h1>My Pokédex</h1>
+        <div id="nav">
+          <router-link to="/">Home</router-link>
+          <router-link to="/about">About</router-link>
+        </div>
+      </div>
     </div>
-    <router-view />
+    <div class="content">
+      <router-view />
+    </div>
+    <div class="footer">
+      <p>
+        <a href="https://github.com/cpartington/my-pokedex" target="blank">Github Repository</a>
+      </p>
+    </div>
   </div>
 </template>
 
 <style>
+body {
+  margin: 0px;
+  padding: 0px;
+  font-family: Roboto, sans-serif;
+  font-size: 16px;
+  color: #414141;
+}
+
+h1,
+h2,
+h3 {
+  font-family: Spectral, serif;
+}
+
+a {
+  color: #d19192;
+  text-decoration: none;
+}
+
+a:hover {
+  text-decoration: underline;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+}
+
+.header-bar {
+  background: lightsteelblue;
+  padding: 10px 100px;
+}
+
+.header-content {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  max-width: 850px;
+  margin: 0 auto;
+}
+
+.header-content h1 {
+  font-size: 2.5em;
 }
 
 #nav {
@@ -23,10 +72,53 @@
 
 #nav a {
   font-weight: bold;
-  color: #2c3e50;
+  color: #414141;
 }
 
 #nav a.router-link-exact-active {
-  color: #42b983;
+  color: #d19192;
+}
+
+#nav a:not(:last-child) {
+  margin-right: 1em;
+}
+
+.content {
+  margin: 10px;
+}
+
+/* Footer */
+.footer {
+  background-color: lightsteelblue;
+  padding: 15px;
+  text-align: center;
+}
+
+.footer a {
+  color: #414141;
+}
+
+.footer p {
+  font-size: 0.8em;
+  margin: 0;
+}
+
+/* Mobile */
+@media only screen and (max-width: 450px) {
+}
+
+/* Desktop */
+@media only screen and (min-width: 451px) {
+  .content {
+    max-width: 850px;
+    margin: auto;
+    margin-bottom: 75px;
+  }
+
+  .footer {
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+  }
 }
 </style>
