b6ad05944a9c46989e34d129dbd06208eec83f19
Christie Partington <christiedpartington@gmail.com>
2020-03-04 13:45:05 -0700

    Remove vue-material imports

--- src/main.js
+++ src/main.js
@@ -2,14 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
-import {
-  MdCard
-} from 'vue-material/dist/components'
-import 'vue-material/dist/vue-material.min.css'
-import 'vue-material/dist/theme/default.css'
-
 Vue.config.productionTip = false
-Vue.use(MdCard)
 
 new Vue({
   router,
