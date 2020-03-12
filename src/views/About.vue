9ecc9f71fc08bc852c13a15603ed8575c7d2b3ef
Christie Partington <christiedpartington@gmail.com>
2020-03-11 23:19:24 -0600

    basic about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,33 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1 class="page-title">About</h1>
+
+    <p>Do you ever struggle to remember what pokémon you&rsquo;ve already caught? Have you tried to manage a spreadsheet of your pokémon so you don&rsquo;t waste time on pokémon you don&rsquo;t need? Are there pokémon you haven&rsquo;t evolved or breeded to increase your collection?</p>
+
+    <p class="statement">Well then this might be the website for you!</p>
+
+    <p><span class="name">My Pokédex</span> helps you keep track of your Sword &amp; Shield Pokédex. In addition to the master list, it also includes suggestions based on what pokémon you&rsquo;ve already caught. Maybe you caught a Grubbin and forgot to evolve it to get Charjabug and Vikavolt. The suggestions page finds all of these cases. This makes completing your pokédex easier and hopefully faster.</p>
+
+    <p>Future goals for the sight include more specific suggestions and user sign-on so you can save your own pokédex.</p>
   </div>
 </template>
+
+<style scoped>
+p {
+  margin-bottom: 1em;
+  font-size: 1.2em;
+}
+
+br {
+  margin: 0.25em;
+}
+
+.statement {
+  font-size: 1.4em;
+  font-weight: 700;
+}
+
+.name {
+  font-style: italic;
+}
+</style>
